b30e410be08d03933b471cb54fc852c4e5499667
Your Name <your-email@example.com>
2026-04-10 12:31:42 +0530

    Initial commit

---
+++ src/main/java/com/dinesh/backend/cloud_monitoring_and_autohealing_platform/CloudMonitoringAndAutoHealingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.backend.cloud_monitoring_and_autohealing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMonitoringAndAutoHealingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMonitoringAndAutoHealingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dinesh/backend/cloud_monitoring_and_autohealing_platform/CloudMonitoringAndAutoHealingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.backend.cloud_monitoring_and_autohealing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMonitoringAndAutoHealingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
